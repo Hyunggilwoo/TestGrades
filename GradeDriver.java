98d1137ab14d83fd831524375b2aaf5b64225c39
Hyunggil Woo <hyunggilswoo@gmail.com>
2021-12-07 12:08:53 -0800

    Adding a method to find the averages of all the students.

--- GradeDriver.java
+++ GradeDriver.java
@@ -11,16 +11,27 @@ import java.util.Scanner;
  *
  */
 public class GradeDriver{
-/**
- * Add appropriate documentation here
- *
- */
+    
+    public static final int NUMBER_OF_EXAMS = 4;
+
+    /**
+     * Accepts a file to scan how many number of averages it will need to compute.
+     * Requires the first line of the input file contain an int > 0.
+     * // Method ignores the number of data entry from the first line.
+     * Creates the TestGrades class from the main driver.
+     *
+     * I don't enjoy this input file. Can I just reorganize the input file into a 
+     * way that I want?
+     *
+     */
 	public static void main(String[] theArgs){
       	Scanner input = null;
       	PrintStream output = null;
         try {
-          input = new Scanner(new File("in4.txt"));
-      		output = new PrintStream(new File("out4.txt"));
+            input = new Scanner(new File("in4.txt"));
+            output = new PrintStream(new File("out4.txt"));
+            numOfStudent(input, output);
+            find(input, output);
         }
         catch (FileNotFoundException e) {
           System.out.println("Error opening file: " + e);
@@ -29,29 +40,42 @@ public class GradeDriver{
     }
     
     /**
+    * Expects the 
     * Method to print out the number of test average of students from a file.
-    *
+    *  
+    * Post: Returns the averages of the exam to a new method.
     */
-    public static TestGrades find() {
-        int count = 0;
-        int number = input.nextInt();
-        while(input.hasNextLine()) {
-            String firstName = input.next();
-            String lastName = input.next();
-            int[] scores = new int[4];
-            for (int i = 0; i < scores.length; i++) {
-                scores[i] = input.nextInt();
+    public static void find(Scanner theConsole, PrintStream theOutput) {
+        while(theConsole.hasNext()) {
+            String firstName = theConsole.next();
+            String lastName = theConsole.next();
+            int[] scores = new int[NUMBER_OF_EXAMS];
+            for (int j = 0; j < scores.length; j++) {
+                scores[j] = theConsole.nextInt();
             }
             TestGrades student = new TestGrades(firstName, lastName, scores);
-            
-            count++;
+            theOutput.println(student.toString());
         }
     }
+    
+    /**
+    * Method that parses the number of student from the input file.
+    *
+    */
+    public static int numOfStudent(Scanner theConsole, PrintStream theOutput) {
+        int numOfStudent = theConsole.nextInt();
+        theOutput.println("Total number of students: " + numOfStudent);
+        return numOfStudent;
+    }
+    
     /**
     * Average of the student's grade should be stored in a separate array.
     * @requires The array size will = the first number from the input file.
     * @return a new array with the same size of an array that contains the average of each students.
     */
+    public static int[] sortAverages() {
+    
+    }
     
     
     /**
