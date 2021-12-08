f6029d6cdbb1d2ef3e0207c30112afbb1da60f43
Hyunggil Woo <hyunggilswoo@gmail.com>
2021-12-07 22:39:39 -0800

    Edit typos.

--- GradeDriver.java
+++ GradeDriver.java
@@ -1,28 +1,35 @@
 /*
- * Add appropriate documentation here
- *
+ * Course: TCSS142 � Introduction to Object-Oriented Programming
+ * File Name: GradeDriver.java
+ * Assignment: 4
+ * Due Date: December 7, 2021
+ * Instructor: Mr. Schuessler
  */
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintStream;
 import java.util.Arrays;
 import java.util.Scanner;
+
 /**
- * Add appropriate documentation here
- *
+ * This program computes the test averages stored in a file.
+ * It relies on a TestGrade class to output the overall average
+ * of the students, and the highest average, lowest average, and
+ * the overall average of the students.
  */
 public class GradeDriver{
 
+    /** Number of Exams are kept here*/
     public static final int NUMBER_OF_EXAMS = 4;
 
     /**
-     * Accepts a file to scan how many number of averages it will need to compute.
-     * Requires the first line of the input file contain an int > 0.
+     * Accepts a file to scan how many number of averages it will need to
+     * compute. Requires the first line of the input file contain an int > 0.
      * // Method ignores the number of data entry from the first line.
      * Creates the TestGrades class from the main driver.
      *
-     *
-     *
+     * @param theArgs accepts the command argument from the console.
      */
     public static void main(String[] theArgs){
         Scanner input = null;
@@ -33,11 +40,11 @@ public class GradeDriver{
             int number = numOfStudent(input, output);
             TestGrades[] studentReport = find(input, number);
             printStudents(output, studentReport);
-            int[] testAverages = generateArray(studentReport);
+            int[] Averages = generateArray(studentReport);
             output.println();
-            output.println("Highest Student Average = " + max(testAverages));
-            output.println("Lowest Student Average = " + min(testAverages));
-            output.println("Overall Class Average = " + overallAverage(testAverages));
+            output.println("Highest Student Average = " + highest(Averages));
+            output.println("Lowest Student Average = " + lowest(Averages));
+            output.println("Overall Class Average = " + overall(Averages));
         }
         catch (FileNotFoundException e) {
             System.out.println("Error opening file: " + e);
@@ -54,16 +61,13 @@ public class GradeDriver{
     public static TestGrades[] find(Scanner theConsole, int theNumber) {
         TestGrades[] studentReport = new TestGrades[theNumber];
         int count = 0;
-        // prints toString() of a student
-        while(theConsole.hasNext()) { // 
-            String firstName = theConsole.next(); // {Sally}
-            String lastName = theConsole.next();  // {Soso}
-            // somehow the every test scores are based on the last student's. error.
+        while(theConsole.hasNext()) {
+            String firstName = theConsole.next();
+            String lastName = theConsole.next();
             int[] scores = new int[NUMBER_OF_EXAMS];
             for (int j = 0; j < scores.length; j++) {
-                scores[j] = theConsole.nextInt(); // { [78, 67, 72, 75]} 
+                scores[j] = theConsole.nextInt();
             }
-            // Sally Soso [78, 67, 72, 75] Average ...
             studentReport[count] = addGrades(firstName, lastName, scores);
             count++;
         }
@@ -71,32 +75,40 @@ public class GradeDriver{
     }
 
     /**
-     * Helper method to add the names and the score into an array of the students.
+     * Helper method to add the names and the score into an array of the
+     * students.
+     *
      * @param theFirstName of a student.
      * @param theLastName of a student.
      * @param theScores on tests.
      * @return TestGrade object.
      */
-    public static TestGrades addGrades (String theFirstName, String theLastName, int[] theScores) {
+    public static TestGrades addGrades (String theFirstName,
+                                        String theLastName, int[] theScores) {
         return new TestGrades(theFirstName, theLastName, theScores);
-
     }
 
     /**
-     * Receives the selection of TestGrade objects, then print all of the students
+     * Receives the selection of TestGrade objects, then print every student
      * that are present in the file.
+     *
      * @param theOutput prints the values into an output file
      * @param theReport is a data that is scanned.
      */
-    public static void printStudents(PrintStream theOutput, TestGrades[] theReport) {
+    public static void printStudents(PrintStream theOutput,
+                                     TestGrades[] theReport) {
         for (TestGrades testGrades : theReport) {
             theOutput.println(testGrades.toString());
         }
     }
 
     /**
-    * Method that parses the number of student from the input file.
+    * If the number of students are printed at the top of input file,
+    * it prints the number of students in this file.
     *
+    * @param theConsole adds an input from a file.
+    * @param theOutput prints it on a file.
+    * @return the number of student.
     */
     public static int numOfStudent(Scanner theConsole, PrintStream theOutput) {
         int numOfStudent = theConsole.nextInt();
@@ -107,48 +119,50 @@ public class GradeDriver{
     /**
      * Generates an array of only the test averages in a sorted way
      * in ascending order.
+     *
      * @param theStudent Array of Students.
      * @return the averages of the test score.
      */
     public static int[] generateArray(TestGrades[] theStudent) {
         int[] averages = new int[theStudent.length];
         for (int i = 0; i < theStudent.length; i++) {
-            averages[i] = theStudent[i].getTestAverage(theStudent[i].getTestScores());
+            averages[i] =
+                theStudent[i].getTestAverage(theStudent[i].getTestScores());
         }
         Arrays.sort(averages);
         return averages;
     }
     
     /**
-    * Inside an array, finds the index of the array that has the highest value.
-    * 
-    * @param theTestAverages contains the average test scores for the individual.
+    * Returns the highest average. max = int[n - 1]
+    *
+    * @param theTestAverages for all students.
     * @return the highest average from the list.
     */
-    public static int max(int[] theTestAverages) {
+    public static int highest(int[] theTestAverages) {
         int size = theTestAverages.length - 1;
         return theTestAverages[size];
     }
     
-    
     /**
-    * Inside an array, finds the index with the lowest value.
-    * if (int[i] < low) { low = int[i] 
-    * @param theTestAverages contains the average test scores for the individual.
+    * Returns the lowest average. min = int[0]
+    *
+    * @param theTestAverages for all students.
     * @return the highest average from the list. 
     */
-    public static int min(int[] theTestAverages) {
+    public static int lowest(int[] theTestAverages) {
         int size = 0;
         return theTestAverages[size];
     }    
     
     /**
-    * Computes the average of the testcores inside the array.
+    * Computes the average of the test scores inside the array.
     * (int[0]+int[1]+...int[n-2]+int[n-1]) / n
-    * @param theTestAverages The average testscores
+    *
+    * @param theTestAverages The average test scores
     * @return overall average test scores for the group of students.
     */
-    public static int overallAverage(int[] theTestAverages) {
+    public static int overall(int[] theTestAverages) {
         int number = theTestAverages.length;
         int sum = 0;
         for (int theTestAverage : theTestAverages) {
--- TestGrades.java
+++ TestGrades.java
@@ -4,20 +4,16 @@
 * Due Date: December 7, 2021
 * Instructor: Professor David Schuessler
 */
-import java.io.*;
+
 import java.util.*;
 
 /**
-* This program prints a the statistics of the class average personality test involving
-* the Keirsey test. When the test results in a text file is scanned, the user can 
-* expect the statistics result of the user's test case.
-* Personality test is first on Extrovert/Introvert 10 questions,
-* Thinking/Feeling 20 questions, Judging/Perceiving 20 questions
-
+* TestGrades class stores the students' first name, last name, and test scores
+* to compute them by the driver of the program.
+*
 * @author Hyunggil Woo
-* @version 2021/12/01
+* @version 2021/12/07
 */
-
 public class TestGrades {
 
     /** Number of exams are accounted here.*/
@@ -26,23 +22,24 @@ public class TestGrades {
     /** Count of students in the constructor.*/
     private static int myStudentCount = 0;
     
-    /** Student's first name is stored here. */
+    /** Student's first name is stored. */
     private String myFirstName;
     
-    /** Student's last name is stored here. */
+    /** Student's last name is stored. */
     private String myLastName;
     
-    /** Scores of 4 tests are stored here. Each index does not contain null value.*/
+    /** Scores of 4 tests are stored.*/
     private int[] myTestScores;
     
-
-    
     /**
-    * TestGrade class contains test values that will create certain values in the beginning.
+    * Constructor of the TestGrade.
     *
-    *
+    * @theFirstName of a student.
+    * @theLastName of a student.
+    * @theTestScores of a student.
     */
-    public TestGrades (String theFristName, String theLastName, int[] theTestScores) {
+    public TestGrades (String theFristName, String theLastName,
+                                                        int[] theTestScores) {
         myFirstName = theFristName;
         myLastName = theLastName;
         myTestScores = theTestScores;
@@ -50,15 +47,14 @@ public class TestGrades {
     }
     
     /**
-    * From the int array, takes the average of the top n - 1 scores in the array.
-    * Find the index with the minimum value. 
+    * Computes the average test scores in the array, except for the 0th index.
+    *
     * @param theTestScores contains the test scores.
-    * @return the average of top 3 scores on the exam.
+    * @return the average of top n - 1 scores on the exam.
     */
     public int getTestAverage(int[] theTestScores) {
         Arrays.sort(theTestScores);
         int number = theTestScores.length;
-        System.out.println(number);
         int total = 0;
         for (int i = 1; i < number; i++) {
             total += theTestScores[i];
@@ -69,6 +65,7 @@ public class TestGrades {
     /**
     * @Requires the test number is greater 0 and less than 5.
     * and the test score must be between 0 and 100.
+    *
     * @param theIndex the place to set the score
     * @param theScore the revised score.
     */
@@ -84,43 +81,48 @@ public class TestGrades {
         }
         myTestScores[theIndex] = theScore;        
     }
-    
-    
+
     /**
-    * Returns the first name of the TestGrades object.
-    * @return myFirstName
+    * Accessor of myFirstName.
+    *
+    * @return copy of myFirstName.
     */
     public String getFirstName() {
         return myFirstName;
     }
     
     /**
-    * Returns the last name of the TestGrades object.
-    * @return the last name of the object.
+    * Accessor of myLastName.
+    *
+    * @return copy of myLastName.
     */    
     public String getLastName() {
         return myLastName;
     }
     
     /**
+    * Accessor of myStudentCount.
     *
-    *
+    * @return copy of myStudentCount.
     */
     public int getStudentCount() {
         return myStudentCount;
     }
+
     /**
-    * A copy of the test grades are printed.
+    * Accessor of myTestScores.
     *
+    * @return copy of myTestScores.
     */
     public int[] getTestScores() {
         return myTestScores;
     }
     
     /**
-    * Returns the above formatted String
-    * First name + last name + list of test scores + the average test scores.
-    * @return 
+    * Returns String of (First name + last name + list of test scores +
+    * the average test scores).
+    *
+    * @return toString format for testGrades.
     */
     public String toString() {
         String output = getFirstName() + " ";
